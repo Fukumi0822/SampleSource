da7565b29736bfd59416268c010daf004813088f
fukumi <rzx2000@gmail.com>
2019-05-10 18:55:30 +0900

    cpp-a-3-1 stringとint型の連結関数の例を追加

--- cpp/a-3/1.cpp
+++ cpp/a-3/1.cpp
@@ -11,13 +11,19 @@ std::string text(std::string a, char b){
 }
 
 std::string text_2(std::string a, std::string b){
+    // stringとstringをinsertで連結
     return a.insert(a.length(), b);
 }
 
 std::string text_2(char a, std::string b){
+    // charとstringをinsertで連結
     return std::string(&a).insert(sizeof(a), b);
 }
 
+std::string text_3(std::string a, int b){
+    // stringとintを連結
+    return a + std::to_string(b);
+}
 
 
 int main(int argc, const char * argv[]) {
@@ -29,6 +35,7 @@ int main(int argc, const char * argv[]) {
     std::cout << "text(string, char) = \t\t\t" << text("Hello", c_World) << "\n";
     std::cout << "text_2(string, string) = \t\t" << text_2("Hello", "World") << "\n";
     std::cout << "text_2(char, string) = \t\t\t" << text_2(c_hello, "World") << "\n";
+    std::cout << "text_3(string, int) = \t\t\t" << text_3("Number", 5) << "\n";
     
     
     return 0;
